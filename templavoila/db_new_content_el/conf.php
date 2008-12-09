76fa73fe42a0db1e1469ea95e8697aae5b0d3344
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-09 22:54:16 +0000

    conf new element wizard
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila_ng/branches/experimental@14772 735d13b6-9817-0410-8766-e36946ffe9aa

--- templavoila/db_new_content_el/conf.php
+++ templavoila/db_new_content_el/conf.php
@@ -1,9 +1,18 @@
 <?php
 
 $MCONF['name']='tx_templavoila_dbnewcontentel';
-
-
-$MCONF['access']='user,group';
 $MCONF['script']='_DISPATCH';
 
+$MCONF['access']='user,group';
+
+if (!strstr($_SERVER['REQUEST_URI'], 'ext/templavoila/mod1/index.php'))
+	return;
+	
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/templavoila/db_new_content_el/');
+$BACK_PATH='../../../../typo3/';
+$MCONF['script'] = 'index.php';
+
+require($BACK_PATH . 'init.php');
+require($BACK_PATH . 'template.php');	
 ?>
