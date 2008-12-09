915c3501e332fe0e96b5f3440d8571709b05796c
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-09 22:07:26 +0000

    removed conflict
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templavoila_ng/branches/experimental@14763 735d13b6-9817-0410-8766-e36946ffe9aa

--- templavoila/ext_tables.php
+++ templavoila/ext_tables.php
@@ -21,13 +21,10 @@ if (TYPO3_MODE=='BE') {
 	t3lib_extMgm::addModulePath('web_txtemplavoilaM2',t3lib_extMgm::extPath ($_EXTKEY).'mod2/');
 	t3lib_extMgm::addModule('web','txtemplavoilaM1','top',t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 	t3lib_extMgm::addModule('web','txtemplavoilaM2','',t3lib_extMgm::extPath($_EXTKEY).'mod2/');
-<<<<<<< .mine
-
-=======
-
+	
 	t3lib_extMgm::addModulePath('tx_templavoila_dbnewcontentel', t3lib_extMgm::extPath ($_EXTKEY) . 'db_new_content_el/');
 
->>>>>>> .r14716
+
 		// Adding backend templates:
 	$GLOBALS['TBE_STYLES']['htmlTemplates']['templates/control-center.html'] =
 		t3lib_extMgm::extRelPath($_EXTKEY).'templates/control-center.html';
